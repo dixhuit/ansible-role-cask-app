e3f043da3c683fbf38f5b1a6d079456f469335a0
Dan Bohea <git@bohea.co.uk>
2018-03-05 13:45:00 +0000

    Tweaks to upgrade_python.sh test script + house keeping

--- tests/upgrade_python.sh
+++ tests/upgrade_python.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+# https://docs.travis-ci.com/user/customizing-the-build/#Implementing-Complex-Build-Steps
+
+set -ev
 if [ "${TRAVIS_OSX_IMAGE}" = "xcode8.3" ]; then
+  # macOS version is 10.12
+  # Upgrade python to avoid SSL errors when using ansible-galaxy to get roles
+  # from GitHub over HTTPS.
   brew update
   brew upgrade python
 fi
